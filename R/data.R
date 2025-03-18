ae514b3fc2ce4e0a3c58c872a5a2fe0b8347ab96
Stephanie Reinders <reinders.stephanie@gmail.com>
2025-03-18 17:52:14 -0500

    docs(data): document csafe_prompt_cfr, cvl_prompt_cfr, and cvl_line_cfc

---
+++ R/data.R
@@ -0,0 +1,164 @@
+#' CSAFE Prompt Cluster Fill Rates Data Frame
+#'
+#' A dataset containing the cluster fill rates of 12,824 handwriting prompts
+#' from the CSAFE Handwriting Database. The variables are as follows:
+#'
+#' @format A data frame with 12824 rows and 46 variables:
+#' \describe{
+#'   \item{docname}{Filename of the handwriting prompt}
+#'   \item{writer}{Writer ID (w0001--w0720)}
+#'   \item{session}{Writing session (s01, s02, s03)}
+#'   \item{prompt}{Writing prompt code (pPHR, pLND, pWOZ)}
+#'   \item{rep}{Repetition number within the given session (r01, r02, r03)}
+#'   \item{total_graphs}{Total number of graphs, or component shapes, into which the handwriting is split}
+#'   \item{cluster1}{Proportion of graphs assigned to cluster1 (0--1)}
+#'   \item{cluster2}{Proportion of graphs assigned to cluster2 (0--1)}
+#'   \item{cluster3}{Proportion of graphs assigned to cluster3 (0--1)}
+#'   \item{cluster4}{Proportion of graphs assigned to cluster4 (0--1)}
+#'   \item{cluster5}{Proportion of graphs assigned to cluster5 (0--1)}
+#'   \item{cluster6}{Proportion of graphs assigned to cluster6 (0--1)}
+#'   \item{cluster7}{Proportion of graphs assigned to cluster7 (0--1)}
+#'   \item{cluster8}{Proportion of graphs assigned to cluster8 (0--1)}
+#'   \item{cluster9}{Proportion of graphs assigned to cluster9 (0--1)}
+#'   \item{cluster10}{Proportion of graphs assigned to cluster10 (0--1)}
+#'   \item{cluster11}{Proportion of graphs assigned to cluster11 (0--1)}
+#'   \item{cluster12}{Proportion of graphs assigned to cluster12 (0--1)}
+#'   \item{cluster13}{Proportion of graphs assigned to cluster13 (0--1)}
+#'   \item{cluster14}{Proportion of graphs assigned to cluster14 (0--1)}
+#'   \item{cluster15}{Proportion of graphs assigned to cluster15 (0--1)}
+#'   \item{cluster16}{Proportion of graphs assigned to cluster16 (0--1)}
+#'   \item{cluster17}{Proportion of graphs assigned to cluster17 (0--1)}
+#'   \item{cluster18}{Proportion of graphs assigned to cluster18 (0--1)}
+#'   \item{cluster19}{Proportion of graphs assigned to cluster19 (0--1)}
+#'   \item{cluster20}{Proportion of graphs assigned to cluster20 (0--1)}
+#'   \item{cluster21}{Proportion of graphs assigned to cluster21 (0--1)}
+#'   \item{cluster22}{Proportion of graphs assigned to cluster22 (0--1)}
+#'   \item{cluster23}{Proportion of graphs assigned to cluster23 (0--1)}
+#'   \item{cluster24}{Proportion of graphs assigned to cluster24 (0--1)}
+#'   \item{cluster25}{Proportion of graphs assigned to cluster25 (0--1)}
+#'   \item{cluster26}{Proportion of graphs assigned to cluster26 (0--1)}
+#'   \item{cluster27}{Proportion of graphs assigned to cluster27 (0--1)}
+#'   \item{cluster28}{Proportion of graphs assigned to cluster28 (0--1)}
+#'   \item{cluster29}{Proportion of graphs assigned to cluster29 (0--1)}
+#'   \item{cluster30}{Proportion of graphs assigned to cluster30 (0--1)}
+#'   \item{cluster31}{Proportion of graphs assigned to cluster31 (0--1)}
+#'   \item{cluster32}{Proportion of graphs assigned to cluster32 (0--1)}
+#'   \item{cluster33}{Proportion of graphs assigned to cluster33 (0--1)}
+#'   \item{cluster34}{Proportion of graphs assigned to cluster34 (0--1)}
+#'   \item{cluster35}{Proportion of graphs assigned to cluster35 (0--1)}
+#'   \item{cluster36}{Proportion of graphs assigned to cluster36 (0--1)}
+#'   \item{cluster37}{Proportion of graphs assigned to cluster37 (0--1)}
+#'   \item{cluster38}{Proportion of graphs assigned to cluster38 (0--1)}
+#'   \item{cluster39}{Proportion of graphs assigned to cluster39 (0--1)}
+#'   \item{cluster40}{Proportion of graphs assigned to cluster40 (0--1)}
+#' }
+"csafe_prompt_cfr"
+
+#' CVL Prompt Cluster Fill Rates Data Frame
+#'
+#' A dataset containing the cluster fill rates of 1,602 handwriting prompts
+#' from the CVL Handwriting Database. The variables are as follows:
+#'
+#' @format A data frame with 1602 rows and 44 variables:
+#' \describe{
+#'   \item{docname}{Filename of the handwriting prompt}
+#'   \item{writer}{Writer ID (0001--1139)}
+#'   \item{prompt}{Writing prompt code (1, 2, 3, 4, 6, 7, 8)}
+#'   \item{total_graphs}{Total number of graphs, or component shapes, into which the handwriting is split}
+#'   \item{cluster1}{Proportion of graphs assigned to cluster1 (0--1)}
+#'   \item{cluster2}{Proportion of graphs assigned to cluster2 (0--1)}
+#'   \item{cluster3}{Proportion of graphs assigned to cluster3 (0--1)}
+#'   \item{cluster4}{Proportion of graphs assigned to cluster4 (0--1)}
+#'   \item{cluster5}{Proportion of graphs assigned to cluster5 (0--1)}
+#'   \item{cluster6}{Proportion of graphs assigned to cluster6 (0--1)}
+#'   \item{cluster7}{Proportion of graphs assigned to cluster7 (0--1)}
+#'   \item{cluster8}{Proportion of graphs assigned to cluster8 (0--1)}
+#'   \item{cluster9}{Proportion of graphs assigned to cluster9 (0--1)}
+#'   \item{cluster10}{Proportion of graphs assigned to cluster10 (0--1)}
+#'   \item{cluster11}{Proportion of graphs assigned to cluster11 (0--1)}
+#'   \item{cluster12}{Proportion of graphs assigned to cluster12 (0--1)}
+#'   \item{cluster13}{Proportion of graphs assigned to cluster13 (0--1)}
+#'   \item{cluster14}{Proportion of graphs assigned to cluster14 (0--1)}
+#'   \item{cluster15}{Proportion of graphs assigned to cluster15 (0--1)}
+#'   \item{cluster16}{Proportion of graphs assigned to cluster16 (0--1)}
+#'   \item{cluster17}{Proportion of graphs assigned to cluster17 (0--1)}
+#'   \item{cluster18}{Proportion of graphs assigned to cluster18 (0--1)}
+#'   \item{cluster19}{Proportion of graphs assigned to cluster19 (0--1)}
+#'   \item{cluster20}{Proportion of graphs assigned to cluster20 (0--1)}
+#'   \item{cluster21}{Proportion of graphs assigned to cluster21 (0--1)}
+#'   \item{cluster22}{Proportion of graphs assigned to cluster22 (0--1)}
+#'   \item{cluster23}{Proportion of graphs assigned to cluster23 (0--1)}
+#'   \item{cluster24}{Proportion of graphs assigned to cluster24 (0--1)}
+#'   \item{cluster25}{Proportion of graphs assigned to cluster25 (0--1)}
+#'   \item{cluster26}{Proportion of graphs assigned to cluster26 (0--1)}
+#'   \item{cluster27}{Proportion of graphs assigned to cluster27 (0--1)}
+#'   \item{cluster28}{Proportion of graphs assigned to cluster28 (0--1)}
+#'   \item{cluster29}{Proportion of graphs assigned to cluster29 (0--1)}
+#'   \item{cluster30}{Proportion of graphs assigned to cluster30 (0--1)}
+#'   \item{cluster31}{Proportion of graphs assigned to cluster31 (0--1)}
+#'   \item{cluster32}{Proportion of graphs assigned to cluster32 (0--1)}
+#'   \item{cluster33}{Proportion of graphs assigned to cluster33 (0--1)}
+#'   \item{cluster34}{Proportion of graphs assigned to cluster34 (0--1)}
+#'   \item{cluster35}{Proportion of graphs assigned to cluster35 (0--1)}
+#'   \item{cluster36}{Proportion of graphs assigned to cluster36 (0--1)}
+#'   \item{cluster37}{Proportion of graphs assigned to cluster37 (0--1)}
+#'   \item{cluster38}{Proportion of graphs assigned to cluster38 (0--1)}
+#'   \item{cluster39}{Proportion of graphs assigned to cluster39 (0--1)}
+#'   \item{cluster40}{Proportion of graphs assigned to cluster40 (0--1)}
+#' }
+"cvl_prompt_cfr"
+
+#' CVL Line Cluster Fill Counts Data Frame
+#'
+#' A dataset containing the cluster fill counts of 13,444 lines handwriting from
+#' prompts from the CVL Handwriting Database. The variables are as follows:
+#'
+#' @format A data frame with 13444 rows and 44 variables:
+#' \describe{
+#'   \item{docname}{Filename of the handwriting prompt}
+#'   \item{writer}{Writer ID (0001--1139)}
+#'   \item{prompt}{Writing prompt code (1, 2, 3, 4, 6, 7, 8)}
+#'   \item{line}{Line number (1--16)}
+#'   \item{1}{Proportion of graphs assigned to cluster1 (0--1)}
+#'   \item{2}{Proportion of graphs assigned to cluster2 (0--1)}
+#'   \item{3}{Proportion of graphs assigned to cluster3 (0--1)}
+#'   \item{4}{Proportion of graphs assigned to cluster4 (0--1)}
+#'   \item{5}{Proportion of graphs assigned to cluster5 (0--1)}
+#'   \item{6}{Proportion of graphs assigned to cluster6 (0--1)}
+#'   \item{7}{Proportion of graphs assigned to cluster7 (0--1)}
+#'   \item{8}{Proportion of graphs assigned to cluster8 (0--1)}
+#'   \item{9}{Proportion of graphs assigned to cluster9 (0--1)}
+#'   \item{10}{Proportion of graphs assigned to cluster10 (0--1)}
+#'   \item{11}{Proportion of graphs assigned to cluster11 (0--1)}
+#'   \item{12}{Proportion of graphs assigned to cluster12 (0--1)}
+#'   \item{13}{Proportion of graphs assigned to cluster13 (0--1)}
+#'   \item{14}{Proportion of graphs assigned to cluster14 (0--1)}
+#'   \item{15}{Proportion of graphs assigned to cluster15 (0--1)}
+#'   \item{16}{Proportion of graphs assigned to cluster16 (0--1)}
+#'   \item{17}{Proportion of graphs assigned to cluster17 (0--1)}
+#'   \item{18}{Proportion of graphs assigned to cluster18 (0--1)}
+#'   \item{19}{Proportion of graphs assigned to cluster19 (0--1)}
+#'   \item{20}{Proportion of graphs assigned to cluster20 (0--1)}
+#'   \item{21}{Proportion of graphs assigned to cluster21 (0--1)}
+#'   \item{22}{Proportion of graphs assigned to cluster22 (0--1)}
+#'   \item{23}{Proportion of graphs assigned to cluster23 (0--1)}
+#'   \item{24}{Proportion of graphs assigned to cluster24 (0--1)}
+#'   \item{25}{Proportion of graphs assigned to cluster25 (0--1)}
+#'   \item{26}{Proportion of graphs assigned to cluster26 (0--1)}
+#'   \item{27}{Proportion of graphs assigned to cluster27 (0--1)}
+#'   \item{28}{Proportion of graphs assigned to cluster28 (0--1)}
+#'   \item{29}{Proportion of graphs assigned to cluster29 (0--1)}
+#'   \item{30}{Proportion of graphs assigned to cluster30 (0--1)}
+#'   \item{31}{Proportion of graphs assigned to cluster31 (0--1)}
+#'   \item{32}{Proportion of graphs assigned to cluster32 (0--1)}
+#'   \item{33}{Proportion of graphs assigned to cluster33 (0--1)}
+#'   \item{34}{Proportion of graphs assigned to cluster34 (0--1)}
+#'   \item{35}{Proportion of graphs assigned to cluster35 (0--1)}
+#'   \item{36}{Proportion of graphs assigned to cluster36 (0--1)}
+#'   \item{37}{Proportion of graphs assigned to cluster37 (0--1)}
+#'   \item{38}{Proportion of graphs assigned to cluster38 (0--1)}
+#'   \item{39}{Proportion of graphs assigned to cluster39 (0--1)}
+#'   \item{40}{Proportion of graphs assigned to cluster40 (0--1)}
+#' }
+"cvl_line_cfc"
+
