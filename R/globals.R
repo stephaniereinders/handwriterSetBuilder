ac3def12405a473dd0630394d29b974a1669672f
Stephanie Reinders <reinders.stephanie@gmail.com>
2025-03-20 12:07:32 -0500

    fix(globals): change "cvl_prompt_cfc" to "cvl_line_cfc"

--- R/globals.R
+++ R/globals.R
@@ -2,4 +2,4 @@
 # note 'no visible binding for global variable' by adding variable <- NULL at
 # the beginning of the function that uses the variable. Instead, we declare the
 # variables here to fix the note.
-utils::globalVariables(c("csafe_prompt_cfr", "cvl_prompt_cfr", "cvl_prompt_cfc"))
+utils::globalVariables(c("csafe_prompt_cfr", "cvl_prompt_cfr", "cvl_line_cfc"))
